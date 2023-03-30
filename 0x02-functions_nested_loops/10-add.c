5769cfc996ec1e1211bf16f0d5bd63f00950d4d0
Iniobong <you@amosiniobong63@gmail.com>
2023-03-29 22:09:00 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sum two integers and return the result
+ * @a: int to be added to b
+ * @b: int to ne added to a
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
