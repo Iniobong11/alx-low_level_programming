40b26189ab45c0c31ec8d24de6e9149a60601a1a
Iniobong <you@amosiniobong63@gmail.com>
2023-05-07 13:14:35 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,11 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t numb = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		numb++;
+		h = h->next;
 	}
 	return (numb);
 }
